379c1daf16dcb50457b8e0e12d4b76bf256948e7
mo <takashi_home_sky@yahoo.co.jp>
2020-07-11 15:11:55 +0900

    データベース作成用ファイルの追加

---
+++ createdatabase.sql
@@ -0,0 +1,12 @@
+drop database if exists nodesample;
+create database nodesample;
+
+use nodesample;
+
+drop table if exists users;
+create table users (
+  id int unsigned primary key auto_increment,
+  mail varchar(225) NOT NULL unique,
+  passward varchar(225) unique,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
