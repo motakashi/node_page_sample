c36ba53e0dfb21bab0d6f1420da08fb24d613056
mo <takashi_home_sky@yahoo.co.jp>
2020-07-12 20:40:48 +0900

    新規ユーザ追加処理を追加

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const express = require('express');
+const util = require('util');
 // const bodypaser = require('body-paser');
 // const session = require('express-session');
 const mysql = require('mysql');
+const bcrypt = require('bcrypt');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 // const moment = require('moment');
 // const uuid = require('uuid');
 
-// const SALT_ROUNDS = 10;
+const SALT_ROUNDS = 10;
 
 // DBの接続情報
 const port = 8000
 const db = mysql.createConnection({
+  connectionLimit: 10,
   host: 'localhost',
   user: 'nodesample',
   password: 'Node-007',
@@ -36,6 +39,26 @@ app.get('/', function(req, res){
 // ユーザ登録画面
 app.get('/regist', function(req, res){
   res.render("regist");
+});
+
+app.post('/regist', async function(req, res){
+  try{
+    // パスワードのハッシュ化
+    var hash = await bcrypt.hash(req.body.password, SALT_ROUNDS);
+    
+    // データの挿入
+    db.query = util.promisify(db.query);
+    const addNewUserSql = "INSERT INTO users SET ?";
+    await db.query(addNewUserSql, { mail: req.body.mail, password: hash });
+    db.end();
+    res.render("regist");
+    console.log("NewUser: { mail: " + req.body.mail+ ", password: " + hash + "}");
+  } catch(err) {
+    if (err.code === "ER_DUP_ENTRY"){
+      res.render("regist", {error: "既に登録されているメールアドレスです"});
+    }
+    console.log("既に登録されているメールアドレス");
+  };
 });
 
 // DBセッションの接続
